ca8dd6ce0e5737ab9f35cecb47e93a17e0edc6fa
Peter Cummuskey <peterc@aetheric.co.nz>
2014-01-15 08:59:45 +1300

    Putting the git config back to the home dir, and adding vim config.

--- install.cmd
+++ install.cmd
@@ -1,6 +1,10 @@
 @ECHO OFF
 
-ECHO Installing git config files to %GIT_HOME%.
-DEL /Q "%GIT_HOME%\etc\gitconfig" "%GIT_HOME%\etc\gitignore"
-MKLINK /H "%GIT_HOME%\etc\gitconfig" "git\gitconfig"
-MKLINK /H "%GIT_HOME%\etc\gitignore" "git\gitignore"
+ECHO Installing git config files to %HOME%.
+DEL /Q "%HOME%\.gitconfig" "%HOME%\.gitignore"
+MKLINK /H "%HOME%\.gitconfig" "git\gitconfig"
+MKLINK /H "%HOME%\.gitignore" "git\gitignore"
+
+ECHO Installing vim config files to %HOME%
+DEL /Q "%HOME%\.vimrc"
+MKLINK /H "%HOME%\.vimrc" "vim\vimrc"
