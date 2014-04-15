573c06f79a05645e396d0a4b5b15adc95295d86e
pcummuskey <pcummuskey@bravurasolutions.com>
2014-04-15 08:55:57 +0000

    Configured vim installation properly.

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ rm -f ~/.bashrc
 cp bash/bashrc ~/.bashrc
 
 # Set up vim
-rm -f ~/.vimrc
+rm -rf ~/.vimrc ~/.vim
 cp vim/vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+sudo apt-get -qq install curl --yes
+curl -Sso ~/.vim/autoload/pathogen.vim \
+	https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 # Set up git
 rm -f ~/.gitconfig ~/.gitignore
