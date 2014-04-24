f953ee14d57c056eecb88d4883e97156fd00f127
pcummuskey <pcummuskey@bravurasolutions.com>
2014-04-24 05:07:32 +0000

    Modifying linux install to add two vim plugins.

--- install.sh
+++ install.sh
@@ -7,10 +7,16 @@ cp bash/bashrc ~/.bashrc
 # Set up vim
 rm -rf ~/.vimrc ~/.vim
 cp vim/vimrc ~/.vimrc
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/autoload
 sudo apt-get -qq install curl --yes
 curl -Sso ~/.vim/autoload/pathogen.vim \
 	https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Set up vim plugins
+rm -rf ~/.vim/bundle
+mkdir ~/.vim/bundle
+(cd ~/.vim/bundle && git clone git://github.com/digitaltoad/vim-jade.git)
+(cd ~/.vim/bundle && git clone https://github.com/pangloss/vim-javascript.git)
 
 # Set up git
 rm -f ~/.gitconfig ~/.gitignore
