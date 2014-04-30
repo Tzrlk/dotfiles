d79467dd76ddd04c056da1c6b8b52630340fa743
default_git_name <default_git_email>
2014-04-30 20:44:56 +0000

    Updating script to not require curl or wget.

--- install.sh
+++ install.sh
@@ -9,24 +9,12 @@ rm -rf ~/.vimrc ~/.vim
 cp vim/vimrc ~/.vimrc
 
 echo '+ Making sure pathogen is set up right.'
+
+(cd ~ && git clone git@github.com:tpope/vim-pathogen.git .vim)
+
 if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
-
-	echo '+ Ensuring autoload directory exists'
-	mkdir -p ~/.vim/autoload
-
-	echo '+ Installing curl'
-	sudo apt-get install curl --yes
-
-	echo '+ Downloading pathogen'
-	basepath=https://raw.githubusercontent.com
-	repopath=$basepath/tpope/vim-pathogen/master
-	filepath=$repopath/autoload/pathogen.vim
-	curl -#So ~/.vim/autoload/pathogen.vim $filepath
-
-	if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
-		echo 'Something went wrong setting up pathogen.'
-		return 0
-	fi
+	echo 'Something went wrong setting up pathogen.'
+	return 0
 fi
 
 echo '+ Preparing plugins directory'
@@ -34,8 +22,8 @@ rm -rf ~/.vim/bundle
 mkdir ~/.vim/bundle
 
 echo '+ Downloading plugins'
-(cd ~/.vim/bundle && git clone git://github.com/digitaltoad/vim-jade.git)
-(cd ~/.vim/bundle && git clone https://github.com/pangloss/vim-javascript.git)
+(cd ~/.vim/bundle && git clone git@github.com:digitaltoad/vim-jade.git)
+(cd ~/.vim/bundle && git clone git@github.com:pangloss/vim-javascript.git)
 (cd ~/.vim/bundle && git clone git@github.com:tpope/vim-markdown.git)
 (cd ~/.vim/bundle && git clone git@github.com:dagwieers/asciidoc-vim.git)
 
