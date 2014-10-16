9b02932f043f54aaaf55280073d61791623369e9
unknown <peterc@aetheric.co.nz>
2014-10-17 09:51:32 +1300

    Adding vim setup to windows install script.

--- install.cmd
+++ install.cmd
@@ -1,10 +1,27 @@
 @ECHO OFF
 
-ECHO Installing git config files to %HOME%.
-DEL /Q "%HOME%\.gitconfig" "%HOME%\.gitignore"
-MKLINK /H "%HOME%\.gitconfig" "git\gitconfig"
-MKLINK /H "%HOME%\.gitignore" "git\gitignore"
+ECHO Installing git config files to %USERPROFILE%.
+DEL /Q "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.gitignore"
+MKLINK /H "%USERPROFILE%\.gitconfig" "git\gitconfig"
+MKLINK /H "%USERPROFILE%\.gitignore" "git\gitignore"
+ECHO .
 
-ECHO Installing vim config files to %HOME%
-DEL /Q "%HOME%\.vimrc"
-MKLINK /H "%HOME%\.vimrc" "vim\vimrc"
+ECHO Installing vim config files to %USERPROFILE%
+DEL /Q "%USERPROFILE%\.vimrc"
+MKLINK /H "%USERPROFILE%\.vimrc" "vim\vimrc"
+ECHO .
+
+ECHO Removing and re-creating vimfiles directory.
+RMDIR /Q /S "%USERPROFILE%\vimfiles"
+MKDIR "%USERPROFILE%\vimfiles"
+ECHO .
+
+ECHO Creating autoload directory and downloading pathogen.
+MKDIR "%USERPROFILE%\vimfiles\autoload"
+curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > "%USERPROFILE%\vimfiles\autoload\pathogen.vim"
+ECHO .
+
+ECHO Creating bundle directory.
+MKDIR "%USERPROFILE%\vimfiles\bundle"
+ECHO .
+
