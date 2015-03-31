c5a22546c4c45a6a3c2556e93daa18bf3cbbb406
pcummuskey <peterc@aetheric.co.nz>
2015-04-01 10:21:41 +1300

    Added npm proxy switching.

--- proxy-bravura.cmd
+++ proxy-bravura.cmd
@@ -3,3 +3,6 @@
 git config --global --replace-all http.proxy http://aukquid2.bravurasolutions.local:3128
 git config --global --replace-all https.proxy http://auksquid2.bravurasolutions.local:3128
 
+CALL npm config -g set proxy http://auksquid2.bravurasolutions.local:3128/
+CALL npm config -g set https-proxy http://auksquid2.bravurasolutions.local:3128/
+
--- proxy-clear.cmd
+++ proxy-clear.cmd
@@ -3,3 +3,6 @@
 git config --global --unset-all http.proxy
 git config --global --unset-all https.proxy
 
+CALL npm config -g delete proxy
+CALL npm config -g delete https-proxy
+
