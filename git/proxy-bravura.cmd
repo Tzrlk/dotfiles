a7516260a0c8016cb0d25881ec9db1a1b7ac5cc7
pcummuskey <peterc@aetheric.co.nz>
2015-04-01 09:44:09 +1300

    Adding proxy modification scripts.

---
+++ git/proxy-bravura.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+git config --global --replace-all http.proxy http://aukquid2.bravurasolutions.local:3128
+git config --global --replace-all https.proxy http://auksquid2.bravurasolutions.local:3128
+
---
+++ git/proxy-clear.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+git config --global --unset-all http.proxy
+git config --global --unset-all https.proxy
+
