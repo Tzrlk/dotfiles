b4386c36b3e8aebbc924d4afe5db1fac69e2a4d8
pcummuskey <peterc@aetheric.co.nz>
2015-08-21 11:24:02 +1200

    Fixing bravura proxy settings.

--- proxy-bravura.cmd
+++ proxy-bravura.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-git config --global --replace-all http.proxy http://aukquid2.bravurasolutions.local:3128
+git config --global --replace-all http.proxy http://auksquid2.bravurasolutions.local:3128
 git config --global --replace-all https.proxy http://auksquid2.bravurasolutions.local:3128
 
 CALL npm config -g set proxy http://auksquid2.bravurasolutions.local:3128/
