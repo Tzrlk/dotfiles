3b5c1dba43337e62452fb223f5eb81ac4910483a
pcummuskey <pcummuskey@bravurasolutions.com>
2017-03-16 10:57:35 +1300

    Updating git config and proxy scripts.

--- proxy-bravura.cmd
+++ proxy-bravura.cmd
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-git config --global --replace-all http.proxy http://auksquid2.bravurasolutions.local:3128
-git config --global --replace-all https.proxy http://auksquid2.bravurasolutions.local:3128
+SET proxy=http://proxy.bravurasolutions.local:3128/
 
-CALL npm config -g set proxy http://auksquid2.bravurasolutions.local:3128/
-CALL npm config -g set https-proxy http://auksquid2.bravurasolutions.local:3128/
+SETX http_proxy "%proxy%"
+SETX https_proxy "%proxy%"
+SETX ftp_proxy "%proxy%"
+SETX no_proxy "127.0.0.1,localhost,bravurasolutions.local"
 
-SETX http_proxy http://auksquid2.bravurasolutions.local:3128/
-SETX https_proxy http://auksquid2.bravurasolutions.local:3128/
-SETX ftp_proxy http://auksquid2.bravurasolutions.local:3128/
+git config --global --replace-all http.proxy %proxy%
+git config --global --replace-all https.proxy %proxy%
 
--- proxy-clear.cmd
+++ proxy-clear.cmd
@@ -3,10 +3,8 @@
 git config --global --unset-all http.proxy
 git config --global --unset-all https.proxy
 
-CALL npm config -g delete proxy
-CALL npm config -g delete https-proxy
-
 SETX http_proxy ""
 SETX https_proxy ""
 SETX ftp_proxy ""
+SETX no_proxy "127.0.0.1,localhost"
 
